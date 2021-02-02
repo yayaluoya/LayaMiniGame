6e1e51aacdc0eaa75beda3275f47de2fae9fff70
yayaluoya <yayaluoya@sina.com>
2021-02-02 10:04:21 +0800

    no message

--- .node/serve/src/main.ts
+++ .node/serve/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   //输出信息
-  console.log('开启服务 http://localhost:3000/');
+  console.log('\n');
+  console.log('----▷ 按 F5 键启动 DeBug 浏览器，并打开 http://localhost:3000/ 地址');
 }
 bootstrap();
