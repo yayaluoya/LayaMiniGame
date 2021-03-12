83ab01eb1c55b5c7aaaeed8b58a21750ddb442ae
YAYALUOYA <yayaluoya@sina.com>
2021-03-12 11:43:27 +0800

    默认不加载工具包

--- bin/index.js
+++ bin/index.js
@@ -10,9 +10,9 @@ window.screenOrientation = "sensor_landscape";
 //
 loadLib("libs/min/pako_inflate.min.js");//解压工具类
 //不必要工具包
-loadLib("libs/min/axios.min.js");//网络请求工具类
-loadLib("libs/min/underscore-min.js");//辅助工具包，提供了100多个辅助函数，用_访问
-loadLib("libs/min/dayjs.min.js");//显示现代浏览器的日期和时间的工具
+// loadLib("libs/min/axios.min.js");//网络请求工具类
+// loadLib("libs/min/underscore-min.js");//辅助工具包，提供了100多个辅助函数，用_访问
+// loadLib("libs/min/dayjs.min.js");//显示现代浏览器的日期和时间的工具
 //-----libs-begin-----
 loadLib("libs/laya.core.js");//laya核心包
 loadLib("libs/laya.d3.js");//laya3d包
