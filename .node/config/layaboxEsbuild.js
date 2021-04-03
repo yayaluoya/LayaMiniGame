6875ec12e49576aad66ea3fd4bf392794d50bdb8
yayaluoya <yayaluoya@sina.com>
2021-04-03 18:33:22 +0800

    更新esbuild

---
+++ .node/config/layaboxEsbuild.js
@@ -0,0 +1,9 @@
+const path = require('path');
+//
+/** 配置数据 */
+module.exports = {
+    //代理目录
+    src: path.resolve(__dirname, '../../src/'),
+    /** bin目录 */
+    bin: path.resolve(__dirname, '../../bin/'),
+};
